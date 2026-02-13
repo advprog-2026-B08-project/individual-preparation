158d79fb79ba446a04b94a726ca63102b9bc3294
IkanBerkepalaLele <ehgojim@gmail.com>
2026-02-13 18:21:50 +0700

    feat: implement arithmetic exponent unit test

---
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
@@ -0,0 +1,42 @@
+package com.example.individualprep.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ArithmeticUtilityTest {
+    
+    double delta = 1e-9;
+
+    @Test
+    void testPositiveExponent() {
+        ArithmeticUtility ArithmeticUtil = new ArithmeticUtility();
+        assertEquals(8.0, ArithmeticUtil.exponent(2, 3), delta);
+    }
+
+    @Test
+    void testNegativeBase() {
+        ArithmeticUtility ArithmeticUtil = new ArithmeticUtility();
+        assertEquals(-8.0, ArithmeticUtil.exponent(-2, 3), delta);
+        assertEquals(4.0, ArithmeticUtil.exponent(-2, 2), delta);
+    }
+
+    @Test
+    void testZeroExponent() {
+        ArithmeticUtility ArithmeticUtil = new ArithmeticUtility();
+        assertEquals(1.0, ArithmeticUtil.exponent(5, 0), delta);
+    }
+
+    @Test
+    void testZeroBase() {
+        ArithmeticUtility ArithmeticUtil = new ArithmeticUtility();
+        assertEquals(0.0, ArithmeticUtil.exponent(0, 5), delta);
+    }
+
+    @Test
+    void testNegativeExponent() {
+        ArithmeticUtility ArithmeticUtil = new ArithmeticUtility();
+        assertEquals(0.25, ArithmeticUtil.exponent(2, -2), delta);
+    }
+}
